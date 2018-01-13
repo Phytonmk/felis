5edd55665dc678dd6496647c84f6ffaf989eadc6
Phytonmk <Phytonmk@users.noreply.github.com>
2018-01-14 01:33:03 +0300

    Revert "Revert "Fixes, time traveling methods and parents subscribers""
    This reverts commit a6d106f9896b5c4be3e06ba6620514a7e351b3e4.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,19 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
+const concat = require('gulp-concat');
 
-gulp.task('build', () => {
-  return gulp.src('src/*.js')
+gulp.task('web', () => {
+  return gulp.src('src/index.js')
+    .pipe(babel({
+        presets: ['env', 'es2015']
+      }))
+    .pipe(uglify())
+    .pipe(gulp.dest('build'))
+});
+gulp.task('exports', () => {
+  return gulp.src(['src/exports.txt', 'src/index.js'])
+    .pipe(concat('entry.js', {newLine: ''}))
     .pipe(babel({
         presets: ['env', 'es2015']
       }))
@@ -11,11 +21,12 @@ gulp.task('build', () => {
     .pipe(gulp.dest('build'))
 });
 gulp.task('debug', () => {
-  return gulp.src('src/*.js')
+  return gulp.src(['src/exports.txt', 'src/index.js'])
+    .pipe(concat('index.js', {newLine: ''}))
     .pipe(babel({
         presets: ['env', 'es2015']
       }))
     .pipe(gulp.dest('debug'))
 });
-gulp.task('watch', () => {gulp.watch('src/*.js', ['build', 'debug']);});
-gulp.task('default', ['build', 'debug']);
+gulp.task('watch', () => {gulp.watch('src/*.js', ['web', 'exports', 'debug']);});
+gulp.task('default', ['web', 'exports', 'debug']);
